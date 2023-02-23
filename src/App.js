c89832867b6939c9562c6d7af0e24426651f022e
Ralf Siewert <ralf.siewert@actyvyst.com>
2023-02-23 11:35:34 +0100

    lifting up onAdopt event

--- src/App.js
+++ src/App.js
@@ -1,23 +1,69 @@
 import React from "react";
 import "./styles.css";
 
-function Animal({ emoji, name }) {
+function Animal({ emoji, name, onAdopt, children }) {
   return (
-    <h2>
-      {emoji} {name}
-    </h2>
+    <section>
+      <h2>
+        {emoji} {name}
+      </h2>
+      <p>{children}</p>
+      <Button onClick={onAdopt}>
+        Adopt <strong>{name}</strong>
+      </Button>
+    </section>
+  );
+}
+
+function Button({ children, onClick }) {
+  return (
+    <button className="button" onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
 export default function App() {
+  function handleAdopt(name) {
+    alert(`${name} has been adopted`);
+  }
   return (
     <main>
       <h1>Animal Shelter</h1>
-      <p>Please adopt our lovely pets!</p>
-      <Animal emoji="🐕" name="Lucky" />
-      <Animal emoji="🐈" name="Mittens" />
-      <Animal emoji="🐢" name="Archibald" />
-      <Animal emoji="🐣" name="Chick Norris" />
+      <h2>Please adopt our lovely pets!</h2>
+      <Animal
+        emoji="🐕"
+        name="Lucky"
+        onAdopt={() => {
+          handleAdopt("Lucky");
+        }}
+      >
+        Very cute dog!
+      </Animal>
+      <Animal
+        emoji="🐈"
+        name="Mittens"
+        onAdopt={() => {
+          handleAdopt("Mittens");
+        }}
+      >
+        <strong>Cute cat!</strong>
+        <em>Really lovely!</em>
+      </Animal>
+      <Animal
+        emoji="🐢"
+        name="Archibald"
+        onAdopt={() => {
+          handleAdopt("Archibald");
+        }}
+      />
+      <Animal
+        emoji="🐣"
+        name="Chick Norris"
+        onAdopt={() => {
+          handleAdopt("Chick Norris");
+        }}
+      />
     </main>
   );
 }
